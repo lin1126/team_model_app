1358549c2b542c983712969905abb04c6e823040
lin1126 <670542311@qq.com>
2021-12-24 00:41:25 +0800

    添加修改密码接口

--- server/api/studentAPI.js
+++ server/api/studentAPI.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { updateStudentInfo } = require('../dbs/student.DB')
+const { updateStudentInfo, updatePwd } = require('../dbs/student.DB')
 const { verifyToken } = require('../../server/middlewares/auth')
 
 app.get('/update', async (req, res) => {
@@ -10,6 +10,25 @@ app.get('/update', async (req, res) => {
   if (isValid.isValid == true) {
     if (isValid.identify == '学生') {
       var doc = await updateStudentInfo(parseInt(req.query._id), req.query._field, req.query._data)
+      res.send(doc)
+    } else {
+      const msg = { isValid: isValid.isValid, info: '权限不足' }
+      res.send(msg)
+    }
+  } else {
+    const msg = { isValid: isValid.isValid, info: isValid.tip }
+    res.send(msg)
+  }
+})
+
+app.post('/updatePwd', async (req, res) => {
+  const { _id, _old, _new } = req.body
+  const token = req.headers.authorization
+  const isValid = verifyToken(token)
+  // token成功时就获取相应信息
+  if (isValid.isValid == true) {
+    if (isValid.identify == '学生') {
+      var doc = await updatePwd(parseInt(_id), _old, _new)
       res.send(doc)
     } else {
       const msg = { isValid: isValid.isValid, info: '权限不足' }
--- server/dbs/student.DB.js
+++ server/dbs/student.DB.js
@@ -1,6 +1,7 @@
 // const mongoose = require('../../utils/mongodb')
 const { student } = require('./loginDB')
 
+// 查找某班级下所有学生
 function findStudentClass(grade, career, cla) {
   return new Promise((resolve, reject) => {
     student.find({ grade: grade, career: career, class: cla }, (err, doc) => {
@@ -17,33 +18,75 @@ function updateStudentInfo(id, field, data) {
     return new Promise((resolve, reject) => {
       student.updateOne({ ID: id }, { email: data }, (err, doc) => {
         if (err) {
-          reject('400')
+          reject(400)
         }
-        resolve('200')
+        resolve(200)
       })
     })
   } else if (field == 'phone') {
     return new Promise((resolve, reject) => {
       student.updateOne({ ID: id }, { phone: data }, (err, doc) => {
         if (err) {
-          reject('400')
+          reject(400)
         }
-        resolve('200')
+        resolve(200)
       })
     })
   } else if (field == 'place') {
     return new Promise((resolve, reject) => {
       student.updateOne({ ID: id }, { place: data }, (err, doc) => {
         if (err) {
-          resolve('400')
+          resolve(400)
         }
-        resolve('200')
+        resolve(200)
+      })
+    })
+  } else if (field == 'password') {
+    return new Promise((resolve, reject) => {
+      student.updateOne({ ID: id }, { password: data }, (err, doc) => {
+        if (err) {
+          resolve(400)
+        }
+        resolve(200)
       })
     })
   }
 }
+// 修改密码
+function updatePwd(id, oldPwd, newPwd) {
+  return new Promise((resolve, reject) => {
+    student.find({ ID: id, password: oldPwd }, async (err, doc) => {
+      if (err) {
+        reject(err)
+      }
+      if (doc.length == 0) {
+        const res = {
+          isTrue: false,
+          msg: '您输入的密码有误！',
+        }
+        resolve(res)
+      } else {
+        const result = await updateStudentInfo(id, 'password', newPwd)
+        if (result == 200) {
+          var res = {
+            isTrue: true,
+            msg: '密码修改成功',
+          }
+        } else {
+          var res = {
+            isTrue: false,
+            msg: '系统错误，请稍后重试！',
+          }
+        }
+        resolve(res)
+      }
+    })
+  })
+}
 
+updatePwd(1631808212211, 21221)
 module.exports = {
   findStudentClass,
   updateStudentInfo,
+  updatePwd,
 }
