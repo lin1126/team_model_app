8be794f7605c680f7dd00a7be25d619098cf15b9
lin1126 <670542311@qq.com>
2022-01-05 23:31:05 +0800

    添加教师教学课程接口

--- server/api/courseAPI.js
+++ server/api/courseAPI.js
@@ -3,7 +3,7 @@
  ***************************************************************/
 const express = require('express')
 const app = express()
-const { findCourse } = require('../dbs/courseRaletionDB.js')
+const { findCourse, findTeachCourse } = require('../dbs/courseRaletionDB.js')
 const { findStudentClass } = require('../../server/dbs/courseDB')
 const { verifyToken } = require('../../server/middlewares/auth')
 
@@ -34,6 +34,25 @@ app.get('/getCourseDetail', async (req, res) => {
   if (isValid.isValid == true) {
     if (isValid.identify == '学生') {
       var doc = await findStudentClass(parseInt(req.query._courseID))
+      res.send(doc)
+    } else {
+      const msg = { isValid: isValid.isValid, info: '权限不足' }
+      res.send(msg)
+    }
+  } else {
+    const msg = { isValid: isValid.isValid, info: isValid.tip }
+    res.send(msg)
+  }
+})
+
+// 获取教室教学课程信息表
+app.get('/getTeachCourse', async (req, res) => {
+  const token = req.headers.authorization
+  const isValid = verifyToken(token)
+  // token成功时就获取相应信息
+  if (isValid.isValid == true) {
+    if (isValid.identify == '教师') {
+      var doc = await findTeachCourse(parseInt(req.query._teacherID))
       res.send(doc)
     } else {
       const msg = { isValid: isValid.isValid, info: '权限不足' }
--- server/dbs/courseRaletionDB.js
+++ server/dbs/courseRaletionDB.js
@@ -10,9 +10,8 @@ const courseRaletionSchema = mongoose.Schema({
 })
 
 const courseRaletion = mongoose.model('Course_raletion_db', courseRaletionSchema, 'course_raletion_db')
-
+// 查询当前学生的课程
 function findCourse(ID) {
-  // 查询当前学生的课程
   return new Promise((resolve, reject) => {
     courseRaletion.aggregate(
       [
@@ -42,13 +41,50 @@ function findCourse(ID) {
   })
 }
 
-// 添加一条关系
-function addRelation(data) {
-  const u = new courseRaletion(data)
-  u.save((err) => {
-    if (err) {
-      console.log(err)
-      return
+// 查询教师教学的课程
+function findTeachCourse(teacherID) {
+  return new Promise((resolve, reject) => {
+    courseRaletion
+      .aggregate([
+        {
+          $match: { teacherID: teacherID },
+        },
+        // 用group分类进行去重
+        {
+          $group: {
+            _id: '$courseID',
+            teacherID: { $first: '$teacherID' },
+          },
+        },
+        {
+          $lookup: {
+            //定义规则
+            from: 'course_db', //在order_item集合中查找
+            localField: '_id', //当前查询的字段
+            foreignField: 'courseID', //对应order_item集合的哪个字段
+            as: 'courseDetail', //在查询结果中键值
+          },
+        },
+        {
+          $project: { teacherID: 1, courseID: 1, courseDetail: { name: 1, class: 1, year: 1, state: 1, Photo: 1 } },
+        },
+      ])
+      .exec((err, doc) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(doc)
+      })
+
+    // 添加一条关系
+    function addRelation(data) {
+      const u = new courseRaletion(data)
+      u.save((err) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+      })
     }
   })
 }
@@ -69,4 +105,5 @@ function addRelation(data) {
 
 module.exports = {
   findCourse,
+  findTeachCourse,
 }
