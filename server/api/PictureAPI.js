f16f39bea291dae46073ef906c1faf09a24e6412
lin1126 <670542311@qq.com>
2022-02-23 16:00:17 +0800

    修改了图片获取的路由和方法

--- server/api/PictureAPI.js
+++ server/api/PictureAPI.js
@@ -4,25 +4,48 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
-// 处理图片
-const readDir = (start, end, skip, limit) => {
+// 按照时间区间处理图片
+const readDirByTime = (start, end, skip, limit) => {
   let dirInfo = fs.readdirSync('./images')
+  dirInfo.reverse()
   let PicDirInfo = []
-  let DirLength = null
   // 遍历所图片
   dirInfo.forEach((item) => {
     // 去掉后缀
     item = item.replace(/[^\d]/g, '')
-    // 遍历文件，将符合条件的文件名存入数组
+    // 遍历文件，将当前时间段中的文件名存入数组
     if (item >= start && item <= end) {
       PicDirInfo.push(item)
     }
   })
-  DirLength = PicDirInfo.length
   // 相当于分页查询，skip代表页数，limit相当于每页的数据条数
   PicDirInfo = PicDirInfo.splice((skip - 1) * limit, limit)
-  return { length: DirLength, PicDirInfo: PicDirInfo }
+  return { length: PicDirInfo.length, PicDirInfo: PicDirInfo }
 }
+
+// 获取图片
+const readDir = (start, end, skip, limit) => {
+  let dirInfo = fs.readdirSync('./images')
+  let PicDirInfo = []
+
+  dirInfo.reverse()
+  dirInfo.forEach((item) => {
+    // 去掉后缀
+    item = item.replace(/[^\d]/g, '')
+    if (item !== '') {
+      PicDirInfo.push(item)
+    }
+  })
+  let totalLength = PicDirInfo.length
+  PicDirInfo = PicDirInfo.splice((skip - 1) * limit, limit)
+  return { length: PicDirInfo.length, PicDirInfo: PicDirInfo, total: totalLength }
+}
+
+app.get('/getForTime', (req, res) => {
+  // readDir(1638023009972, 1638026141389,)
+  const doc = readDirByTime(req.query._start, req.query._stop, req.query._skip, req.query._limit)
+  res.send(doc)
+})
 
 app.get('/get', (req, res) => {
   // readDir(1638023009972, 1638026141389,)
